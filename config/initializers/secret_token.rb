79cbf54608d1c14870d8c6c3b178d856295c1bc2
Sriram Chandrasekaran <sriram441@gmail.com>
2014-02-24 17:39:58 +0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Tuesdayapp::Application.config.secret_key_base = '5ae2d7d4544b60d66ab9f73afa59c3782b1e8f598ca791f7a95f9e809068c3439265cf0d6d164802c82e996a6bf3b3f6297ff60fa175c1b82634ccbaab083c8d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Tuesdayapp::Application.config.session_store :cookie_store, key: '_tuesdayapp_session'
